2a3a29f33a86260b7c6a3326cdb06cd6ee6ca5e1
nolngo <nolanngo@protonmail.com>
2020-11-11 22:53:44 -0800

    added import file

---
+++ dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    def __init__(self,name,breed):
+        self.name = name
+        self.breed = breed
+        print("dog initialized!")
+
+    def bark(self):
+        print("Arf!")
+
+#This call sets the parameter "name" to "Rex"
---
+++ my_dog.py
@@ -0,0 +1,4 @@
+import dog
+
+my_dog = dog.Dog("Earl", "Beagle")
+my_dog.bark()
