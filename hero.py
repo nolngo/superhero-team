7b1752116a5c71a8f8a76b6de120328c3030f626
nolngo <nolanngo@protonmail.com>
2020-11-17 18:18:32 -0800

    ability and attack

--- hero.py
+++ hero.py
@@ -14,10 +14,19 @@ class Hero:
         self.abilities.append(ability)
 
     def attack(self):
-        pass
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
 
-    def defend(self, incoming_damage):
-        self.incoming_damage = incoming_damage
+    def add_armor(self, armor):
+        self.armor.append(armor)
+
+    def defend(self):
+        blocked_damage = 0
+        for armor in self.armor:
+            blocked_damage -= armor.block()
+        return blocked_damage
 
     def take_damage(self, damage):
         self.damage = damage
@@ -32,7 +41,16 @@ class Hero:
 
 
 if __name__ == "__main__":
-    ability = Ability("Great Debugging", 50)
     hero = Hero("Grace Hopper", 200)
+
+    ability = Ability("Tummy Rub", 50)
+    another_ability = Ability("Spank Cheeks", 90)
     hero.add_ability(ability)
-    print(hero.abilities)
+    hero.add_ability(another_ability)
+    print(hero.attack())
+
+    armor = Armor("Chainmail", 100)
+    another_armor = Armor("Paper Helmet", 20)
+    hero.add_armor(armor)
+    hero.add_armor(another_armor)
+    print(hero.defend())
